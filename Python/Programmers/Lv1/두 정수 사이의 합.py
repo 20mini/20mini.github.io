3ed184fce4041200f1c5d48dc467c28258af0407
20mini <dlqslek10@gmail.com>
2022-10-03 00:50:11 +0900

    Update 두 정수 사이의 합.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12912

--- Python/Programmers/Lv1/두 정수 사이의 합.py
+++ Python/Programmers/Lv1/두 정수 사이의 합.py
@@ -1,8 +1,5 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12912
 
 def solution(a, b):
-    if b>a:
-        answer = (a+b)*(b-a+1)/2
-    else:
-        answer = (a+b)*(a-b+1)/2
+    answer = (a+b)/2*(abs(b-a)+1)
     return answer
