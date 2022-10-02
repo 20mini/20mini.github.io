cd23358410a5fcdb3ee452378992e455676c69ff
20mini <dlqslek10@gmail.com>
2022-10-03 04:11:10 +0900

    Update 짝수와 홀수.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=python3

--- Python/Programmers/Lv1/짝수와 홀수.py
+++ Python/Programmers/Lv1/짝수와 홀수.py
@@ -1,8 +1,4 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=python3
 
 def solution(num):
-    if(num%2==0):
-        answer="Even"
-    else:
-        answer="Odd"
-    return answer
+    return "Even" if num%2==0 else "Odd"
