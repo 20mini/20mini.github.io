72f161b27f85cafb6446c3da7356510df480cc10
20mini <dlqslek10@gmail.com>
2022-10-03 05:00:03 +0900

    Update 문자열 내 p와 y의 개수.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=python3

--- Python/Programmers/Lv1/문자열 내 p와 y의 개수.py
+++ Python/Programmers/Lv1/문자열 내 p와 y의 개수.py
@@ -1,13 +1,10 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12916?language=python3
 
 def solution(s):
-    answer = True
     num_p, num_y= 0,0
     for i in s:
         if i=='p' or i=='P':
             num_p+=1
         elif i=='y' or i=='Y':
             num_y+=1
-    if num_p!=num_y:
-        answer = False
-    return answer
+    return num_p==num_y
