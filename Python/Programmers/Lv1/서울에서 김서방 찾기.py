611b4d5e6e10a4b696bd71c985e9c040007df6fe
20mini <dlqslek10@gmail.com>
2022-10-03 01:54:56 +0900

    Update 서울에서 김서방 찾기.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12919

--- Python/Programmers/Lv1/서울에서 김서방 찾기.py
+++ Python/Programmers/Lv1/서울에서 김서방 찾기.py
@@ -1,10 +1,8 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12919
 
 def solution(seoul):
-    n=0
     for i in range(0,len(seoul)):
         if seoul[i] == "Kim":
-            n=i
             break
-    answer = '김서방은 '+str(n)+'에 있다'
+    answer = '김서방은 '+str(i)+'에 있다'
     return answer
