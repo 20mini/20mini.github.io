7c6449193eea68649dcd9897acd44e139c977fb6
20mini <dlqslek10@gmail.com>
2022-10-03 03:56:41 +0900

    Update 제일 작은 수 제거하기.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=python3

--- Python/Programmers/Lv1/제일 작은 수 제거하기.py
+++ Python/Programmers/Lv1/제일 작은 수 제거하기.py
@@ -1,12 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12935?language=python3
 
 def solution(arr):
-    answer=arr
-    min=answer[0]
-    for i in answer:
-        if min > i:
-            min=i
-    answer.remove(min)
-    if len(answer)==0:
-        answer.append(-1)
-    return answer
+    arr.remove(min(arr))
+    if not arr:
+        arr.append(-1)
+    return arr
