70ae7964dafdc78039ede8d66289b7365fbf7636
20mini <dlqslek10@gmail.com>
2022-10-03 02:28:21 +0900

    Create 나누어 떨어지는 숫자 배열.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=python3

---
+++ Python/Programmers/Lv1/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/12910?language=python3
+
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i%divisor==0:
+            answer.append(i)
+    if len(answer)==0:
+        answer.append(-1)
+    else:
+        answer.sort()
+    return answer
