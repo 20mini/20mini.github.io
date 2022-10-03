1283589f7fb54819980c5c4983baefcd11e55a91
20mini <dlqslek10@gmail.com>
2022-10-03 14:22:25 +0900

    Update 가운데 글자 가져오기.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=python3

--- Python/Programmers/Lv1/가운데 글자 가져오기.py
+++ Python/Programmers/Lv1/가운데 글자 가져오기.py
@@ -1,4 +1,4 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/12903
+#https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=python3
 
 def solution(s):
     if len(s)%2==1:
