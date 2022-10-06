c755b95aa7e0589e02ed1d93c2e82b963abd9e5e
20mini <dlqslek10@gmail.com>
2022-10-07 01:15:19 +0900

    Update 부족한 금액 계산하기.py
    https://school.programmers.co.kr/learn/courses/30/lessons/82612

--- Python/Programmers/Lv1/부족한 금액 계산하기.py
+++ Python/Programmers/Lv1/부족한 금액 계산하기.py
@@ -1,7 +1,7 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/82612
 
 def solution(price, money, count):
-    answer=(2*price+price*(count-1))*count/2-money
-    if answer<=0:
-        answer=0
-    return answer
+    cost=(2*price+price*(count-1))*count/2
+    if cost<=money:
+        return 0
+    return cost-money
