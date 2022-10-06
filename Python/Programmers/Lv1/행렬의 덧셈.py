83bc2df0bfbd76883c5ebff980269ca22033e42b
20mini <dlqslek10@gmail.com>
2022-10-06 14:30:01 +0900

    Update 행렬의 덧셈.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12950

--- Python/Programmers/Lv1/행렬의 덧셈.py
+++ Python/Programmers/Lv1/행렬의 덧셈.py
@@ -8,3 +8,13 @@ def solution(arr1, arr2):
             row.append(arr1[i][j]+arr2[i][j])
         answer.append(row)
     return answer
+
+'''
+# 이렇게도 가능
+
+import numpy as np
+
+def solution(arr1, arr2):
+    return (np.array(arr1)+np.array(arr2)).tolist()
+
+'''
