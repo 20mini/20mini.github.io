4a9ff5220e8c99bec464c741c435fde544d30ff9
20mini <dlqslek10@gmail.com>
2022-10-07 21:36:33 +0900

    Update 최대공약수와 최소공배수.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12940

--- Python/Programmers/Lv1/최대공약수와 최소공배수.py
+++ Python/Programmers/Lv1/최대공약수와 최소공배수.py
@@ -1,8 +1,8 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12940
 
 def solution(n, m):
-    for i in range(1,min(n,m)+1):
-        if n%i==0 and m%i==0:
-            GCD=i
+    for GCD in range(min(n,m),0,-1):
+        if n%GCD==0 and m%GCD==0:
+            break
     LCM=n*m/GCD
     return [GCD,LCM]
