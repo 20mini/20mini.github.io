707be0f8ac6e083e58752dd18a53cf3a3a5ae77d
20mini <dlqslek10@gmail.com>
2022-10-12 23:18:54 +0900

    Create 3진법 뒤집기.py
    https://school.programmers.co.kr/learn/courses/30/lessons/68935

---
+++ Python/Programmers/Lv1/3진법 뒤집기.py
@@ -0,0 +1,12 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/68935
+
+def solution(n):
+    arr=[]
+    while n>0:
+        arr.append(n%3)
+        n//=3
+    answer=0
+    print(arr)
+    for i, k in enumerate(arr):
+        answer+=k*(3**(len(arr)-1-i))
+    return answer
