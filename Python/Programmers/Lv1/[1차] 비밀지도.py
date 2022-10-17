7209c016ae74fa0faf0c040d76d81a82aaf45e40
20mini <dlqslek10@gmail.com>
2022-10-17 20:28:16 +0900

    Create [1차] 비밀지도.py
    https://school.programmers.co.kr/learn/courses/30/lessons/17681

---
+++ Python/Programmers/Lv1/[1차] 비밀지도.py
@@ -0,0 +1,17 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/17681
+
+def solution(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        result=bin(i|j)[2:]
+        result=result.rjust(n,'0')
+        answer.append(result.replace('1','#').replace('0',' '))
+    return answer
+
+'''
+def solution(n, arr1, arr2):
+    answer = []
+    for i,j in zip(arr1,arr2):
+        answer.append(bin(i|j)[2:].rjust(n,'0').replace('1','#').replace('0',' '))
+    return answer
+'''
