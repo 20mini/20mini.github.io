b1559600fb22b28ac7b9a95989f785e8c25b9043
20mini <dlqslek10@gmail.com>
2022-10-21 19:42:04 +0900

    Update [1차] 비밀지도.py
    https://school.programmers.co.kr/learn/courses/30/lessons/17681

--- Python/Programmers/Lv1/[1차] 비밀지도.py
+++ Python/Programmers/Lv1/[1차] 비밀지도.py
@@ -3,7 +3,7 @@
 def solution(n, arr1, arr2):
     answer = []
     for i,j in zip(arr1,arr2):
-        result=bin(i|j)[2:]
+        result=bin(i|j)[2:] # 비트 연산(or) 이용
         result=result.rjust(n,'0')
         answer.append(result.replace('1','#').replace('0',' '))
     return answer
