73c5f9d90fb262e4d88b21c55e8532163dd81694
20mini <dlqslek10@gmail.com>
2022-10-28 03:28:20 +0900

    Update 숫자의 표현.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12924

--- Python/Programmers/Lv1/숫자의 표현.py
+++ Python/Programmers/Lv1/숫자의 표현.py
@@ -2,10 +2,10 @@
 
 def solution(n):
     answer=0
-    i, _sum= 1, 0 # i는 연속된 자연수들의 개수, _sum은 1부터 i까지의 합
+    i, _sum= 1, 0 # i는 연속된 자연수들의 개수, _sum은 0부터 i-1까지의 합
     while _sum<n:
-        _sum+=i
-        if (n - _sum)%i==0: # 연속된 자연수 i개가 1+k, 2+k, 3+k, ... , i+k 의 형태일 경우 (k는 자연수)
+        if (n - _sum)%i==0: # 연속된 자연수 i개가 k, 1+k, 2+k, ... , i-1+k 의 형태일 경우 (k는 자연수)
             answer+=1
+        _sum+=i
         i+=1
     return answer
