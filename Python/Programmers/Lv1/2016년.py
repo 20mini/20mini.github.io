86c55be00df20f942568e03bdfec8d4a1f926a67
20mini <dlqslek10@gmail.com>
2022-10-28 04:34:04 +0900

    Update 2016년.py
    https://school.programmers.co.kr/learn/courses/30/lessons/12901

--- Python/Programmers/Lv1/2016년.py
+++ Python/Programmers/Lv1/2016년.py
@@ -1,10 +1,10 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12901
 
 def solution(a, b):
-    month=[0,31,29,31,30,31,30,31,31,30,31,30,31]
-    day_of_the_week=['THU','FRI','SAT','SUN','MON','TUE','WED']
-    gap=0
+    month=[0,31,29,31,30,31,30,31,31,30,31,30,31] # 각 달의 일 수
+    day_of_the_week=['FRI','SAT','SUN','MON','TUE','WED','THU']
+    gap=0 # 1월 1일과 입력 받은 날의 차이
     for i in range(1,a):
         gap+=month[i]
-    gap+=b
+    gap+=b-1
     return day_of_the_week[gap%7]
