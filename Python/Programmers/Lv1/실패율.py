ab3ccadb590a7023b8cf0b1d9fbefd964a1dc073
20mini <dlqslek10@gmail.com>
2022-10-28 19:48:00 +0900

    Update 실패율.py
    https://school.programmers.co.kr/learn/courses/30/lessons/42889

--- Python/Programmers/Lv1/실패율.py
+++ Python/Programmers/Lv1/실패율.py
@@ -1,17 +1,17 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42889
 
 def solution(N, stages):
-    pos=[0]*(N+1)
+    stage_player_num=[0]*(N+1) # 각 스테이지(1~N)에 멈춰있는 사람의 수
     fail_ratio=[0]*(N+1)
 
     for i in stages:
         if i!=N+1:
-            pos[i]+=1
+            stage_player_num[i]+=1
             
-    prev=0
+    prev=0 # i stage에 도달하지 못 한 사람의 수
     for i in range(1,N+1):
-        fail_ratio[i]=pos[i]/(len(stages)-prev)
-        prev+=pos[i]
+        fail_ratio[i]=stage_player_num[i]/(len(stages)-prev)
+        prev+=stage_player_num[i]
         if prev==len(stages):
             break
             
